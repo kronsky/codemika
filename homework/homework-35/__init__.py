53abd5ea6690e3bf1cdb46a69435e664de27488c
kronsky <kronsky73@gmail.com>
2022-03-28 00:08:43 +0300

    homework-35 Не доделана функция удаления

--- homework/homework-35/__init__.py
+++ homework/homework-35/__init__.py
@@ -9,55 +9,71 @@ bot = telebot.TeleBot(config.telegram_token)
 @bot.message_handler(commands=['start'])
 def start(message):
     menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton('/get',)
-    menu.add(button)
+    button_get = types.KeyboardButton('/get',)
+    button_del = types.KeyboardButton('/del', )
+    menu.add(button_get, button_del)
     bot.send_message(message.chat.id, text='...', reply_markup=menu)
 
 
 @bot.message_handler(commands=['get'])
 def get(message):
     files_list = filestore.get_files_list()
+    buttons = types.InlineKeyboardMarkup()
     for file in files_list:
-        bot.send_message(message.chat.id, file['filename'])
+        callback = 'get:' + file['filename']
+        buttons.add(telebot.types.InlineKeyboardButton(text=file['filename'], callback_data=callback))
+    bot.send_message(message.chat.id, text="Какой файл отправить?", reply_markup=buttons)
+
+
+@bot.message_handler(commands=['del'])
+def delete(message):
+    files_list = filestore.get_files_list()
+    buttons = types.InlineKeyboardMarkup()
+    for file in files_list:
+        callback = 'del:' + file['filename']
+        buttons.add(telebot.types.InlineKeyboardButton(text=file['filename'], callback_data=callback))
+    bot.send_message(message.chat.id, text="Какой файл удалить?", reply_markup=buttons)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    if call.data[:3] == 'get':
+        files_list = filestore.get_files_list()
+        for file in files_list:
+            if file['filename'] == call.data[4:]:
+                bot.send_document(call.message.chat.id, file['file_id'])
+    elif call.data[:3] == 'del':
+        print('DEL: функция не доделана')
+    # убираем кнопки
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
 
 @bot.message_handler(content_types=['text'])
-def get_random_image(message):
+def text(message):
     files_list = filestore.get_files_list()
     for file in files_list:
         if file['filename'] == message.text:
-            print(file)
             bot.send_document(message.chat.id, file['file_id'])
 
 
 @bot.message_handler(content_types=['document'])
-def up_file(message):
+def up_document(message):
     document_id = message.document.file_id
     filename = message.document.file_name
     caption = message.caption
     file_info = bot.get_file(document_id)
     if caption:
-        print(f'http://api.telegram.org/file/bot{config.telegram_token}/{file_info.file_path}')
-        bot.send_message(message.chat.id, 'Файл ' + filename + ' сохранён')
+        # print(f'http://api.telegram.org/file/bot{config.telegram_token}/{file_info.file_path}')
+        bot.send_message(message.chat.id, 'Файл ' + filename + ' сохранён. ' + 'Ссылка на файл: ' +
+                         f'http://api.telegram.org/file/bot{config.telegram_token}/{file_info.file_path}')
         filestore.write_file_info(document_id, filename, caption)
     else:
-        bot.send_message(message.chat.id, 'Не указана подпись файла, укажите!')
+        bot.send_message(message.chat.id, 'Не указана подпись файла!')
 
 
 @bot.message_handler(content_types=['photo'])
-def up_file(message):
-    print(message.json.photo.file_id)
-    # document_id = message.document.file_id
-    # filename = message.document.file_name
-    # caption = message.caption
-    # file_info = bot.get_file(document_id)
-    # if caption:
-    #     print(f'http://api.telegram.org/file/bot{config.telegram_token}/{file_info.file_path}')
-    #     bot.send_message(message.chat.id, 'Файл ' + filename + ' сохранён')
-    #     filestore.write_file_info(document_id, filename, caption)
-    # else:
-    #     bot.send_message(message.chat.id, 'Не указана подпись файла, укажите!')
+def up_photo(message):
+    bot.send_message(message.chat.id, 'Отправь фото документом без сжатия!')
 
 
 bot.polling(none_stop=True)
-
